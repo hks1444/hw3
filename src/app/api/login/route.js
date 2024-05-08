fdb9cf95dbccc0d10428454d3a371a4ae44d6d71
hks1444 <hasan-kerem2003@hotmail.com>
2024-05-08 19:54:29 +0300

    sql connection

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,11 +1,28 @@
 import { NextResponse } from "next/server";
-
+import mysql from 'mysql2';
 export async function POST(req, res) {
   const data = await req.json();
   const username = data.username;
   const password = data.password;
   const role = "coach";
   console.log(username, password);
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '19932003',
+    database: 'project3'
+  });
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    let sqlSorgusu = `SELECT * FROM coach`;
+    console.log('MySQL bağlantısı başarıyla gerçekleştirildi.');
+    connection.query(sqlSorgusu, function (err, results, fields) {
+      if (err) throw err;
+      console.log(results);
+    });
+
+  });
   if (username === 'admin' && password === 'admin') {
     return NextResponse.json({ username, password, role }, { status: 200 });
   } else {
